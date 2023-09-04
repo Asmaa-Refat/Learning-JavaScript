3412c68317a3f71bdb0b8bf036c4b15da91f115d
Asmaa-Refat <asmaarefat071@gmail.com>
2023-09-04 22:49:51 +0300

    done

--- Assignment-57-70/main.js
+++ Assignment-57-70/main.js
@@ -156,3 +156,101 @@ multiply(100.5, 10, "B"); // 1000
 /* --------------------------------------------------------------------------------------------- */
 // 064 - 070
 //  التكليف 01
+console.log("-----------------------------------------------------------------------------------------");
+
+function getDetails(zName, zAge, zCountry) {
+    function namePattern(zName) {
+      // Osama Mohamed => Osama M.
+      // Ahmed ali => Ahmed A.
+      let splitted = zName.split(' ');
+      return `${splitted[0]} ${splitted[1][0].toUpperCase()}.`;
+    }
+    function ageWithMessage(zAge) {
+      // 38 Is My Age => Your Age Is 38
+      // 32 Is The Age => Your Age Is 32
+      let age = parseInt(zAge);
+      return `Your Age is ${age}`;
+    }
+    function countryTwoLetters(zCountry) {
+      // Egypt => You Live In EG
+      // Syria => You Live In SY
+      return `You Live In ${zCountry[0]}${zCountry[1].toUpperCase()}`
+    }
+    function fullDetails() {
+        return `Hello ${namePattern(zName)}, ${ageWithMessage(zAge)}, ${countryTwoLetters(zCountry)}`;
+    }
+    return fullDetails(); // Do Not Edit This
+}
+
+console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+  // Hello Osama M., Your Age Is 38, You Live In EG
+
+console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+  // Hello Ahmed A., Your Age Is 32, You Live In SY
+
+/* --------------------------------------------------------------------------------------------- */
+//  التكليف 02
+console.log("-----------------------------------------------------------------------------------------");
+
+//regular function
+function itsMe() {
+    return `Iam A Normal Function`;
+}
+
+//arrow function
+let itsMe2 = _ => `Iam A Normal Function`;
+
+console.log(itsMe2()); // Iam A Normal Function
+
+//regular function
+function urlCreate(protocol, web, tld) {
+    return `${protocol}://www.${web}.${tld}`;
+}
+
+//arrow function
+let urlCreate2 = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+console.log(urlCreate2("https", "elzero", "org")); // https://www.elzero.org
+/* --------------------------------------------------------------------------------------------- */
+//  التكليف 03
+console.log("-----------------------------------------------------------------------------------------");
+
+//regular function
+function checker(zName) {
+    return function (status) {
+      return function (salary) {
+        return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+      };
+    };
+}
+
+// arrow function - WOOOOOW!!!!!! how convenient!
+let checker1 = zName => status => salary => "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+
+console.log(checker1("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+console.log(checker1("Ahmed")("Not Available")()); // Iam Not Avaialble
+/* --------------------------------------------------------------------------------------------- */
+
+//  التكليف 04
+console.log("-----------------------------------------------------------------------------------------");
+function specialMix(...data) {
+    let res = 0;
+    for (const item of data){
+        if(typeof item === "number")
+            res += item;
+        else {
+            let num = parseInt(item);
+            if(!isNaN(num))
+            {
+                res += parseInt(item);
+            }
+        }
+    }
+    if(res === 0)
+        return "All Is Strings";
+    return res;
+}
+
+console.log(specialMix(10, 20, 30)); // 60
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
