6e865d2ffb2b47ea02afc8d99b117786166c98e6
Asmaa-Refat <asmaarefat071@gmail.com>
2023-09-14 16:52:11 +0300

    edit again

--- Assignment-134-146/main.js
+++ Assignment-134-146/main.js
@@ -73,8 +73,7 @@ let url3 = 'https://elzero.org';
 let url4 = 'https://www.elzero.org';
 let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
 
-let re6 = /(https?:\/\/(www.)?)?\w{6}.\w{3}(:\d{4}\/\w{8}.\w{3}\?id=\d{3}&cat=\w{6})?/ig; // Write Your Pattern Here
-
+let re6 = /(https?:\/\/(www.)?)?\w+\.\w+(:\d{4}\/\w+.\w+\?id=\d+&\w+=\w+)?/ig; // Write Your Pattern Here
 console.log(url1.match(re6));
 console.log(url2.match(re6));
 console.log(url3.match(re6));
